c5eaa0c405ee45a447111eab0e403503148c4a8a
Annie Bjurman <id19abn@cs.umu.se>
2021-02-27 20:13:30 +0100

    js complete

--- member_card_animation.js
+++ member_card_animation.js
@@ -1,25 +1,23 @@
-$(document).ready(function () {
-    $("#b-animate").click(function () {
-        $("#member-card").animate({
-            height: '600px',
-            opacity: '100%'
-        });
-        
-        $("#card-header").animate({
-            fontSize: '40px'
-        });
-        
-        $("#card-img").animate({
-            width: '40%'
-        });
-        
-        $("#card-p").animate({
-            fontSize: '15px'
-        });
-        
-        $("#card-img2").animate({
-            width: '90%',
-            marginTop: '50px'
-        });
+$("#b-animate").click(function () {
+    $("#member-card").animate({
+        height: '600px',
+        opacity: '100%'
+    });
+
+    $("#card-header").animate({
+        fontSize: '40px'
+    });
+
+    $("#card-img").animate({
+        width: '40%'
+    });
+
+    $("#card-p").animate({
+        fontSize: '15px'
+    });
+
+    $("#card-img2").animate({
+        width: '90%',
+        marginTop: '50px'
     });
 });
--- toggle-info.js
+++ toggle-info.js
@@ -1,5 +1,3 @@
-$(document).ready(function () {
-    $("#cake-img").click(function () {
-        $("#cake-info").slideToggle();
-    });
+$("#cake-img").click(function () {
+    $("#cake-info").slideToggle();
 });
